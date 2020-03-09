a4a81dddb0ab833105551b3705f9c65eb59441d7
Zack Hayden <signbarn@gmail.com>
2020-03-09 02:09:49 -0400

    countdown timer working now

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,17 @@
 var currentQuizObject;
 var quizObjects = [];
+var highScores = [];
+
+class highScore {
+    constructor(initials, score) {
+        this.initials = initials;
+        this.highscore = score;
+    }
+}
 var answerButtonElements = [document.getElementById("choice-1"), document.getElementById("choice-2"),
     document.getElementById("choice-3"), document.getElementById("choice-4")
 ];
+var pageHeadingElement = document.getElementById("page-heading");
 var questionTextElement = document.getElementById("quiz-question");
 var inCorrectElement = document.getElementById("answer-in-correct");
 var inCorrectTextElement = document.getElementById("in-correct-text");
@@ -14,12 +23,13 @@ var inCorrectTimeout;
 
 function setTime() {
     var timerInterval = setInterval(function () {
-        timeLeftElement.textContent = "Time: " + --secondsLeft;
-        if (secondsLeft <= 0) {
+        if (--secondsLeft <= 0) {
             clearInterval(timerInterval);
+            timeLeftElement.textContent = "Time: 0";
             endQuiz();
+        } else {
+            timeLeftElement.textContent = "Time: " + secondsLeft;
         }
-
     }, 1000);
 }
 
@@ -35,15 +45,88 @@ var questionOne = {
     isCorrect: function (choice) {
         return (choice == this.correct);
     }
-
+}
+var questionTwo = {
+    question: "What kind of function is passed as a parameter to another function?",
+    options: ["void", "static", "callback", "banana"],
+    correct: 2,
+    isCorrect: function (choice) {
+        return (choice == this.correct);
+    }
+}
+var questionThree = {
+    question: "What kind of function is passed as a parameter to another function?",
+    options: ["void", "static", "callback", "banana"],
+    correct: 2,
+    isCorrect: function (choice) {
+        return (choice == this.correct);
+    }
+}
+var questionFour = {
+    question: "What kind of function is passed as a parameter to another function?",
+    options: ["void", "static", "callback", "banana"],
+    correct: 2,
+    isCorrect: function (choice) {
+        return (choice == this.correct);
+    }
+}
+var questionFive = {
+    question: "What kind of function is passed as a parameter to another function?",
+    options: ["void", "static", "callback", "banana"],
+    correct: 2,
+    isCorrect: function (choice) {
+        return (choice == this.correct);
+    }
+}
+var questionSix = {
+    question: "What kind of function is passed as a parameter to another function?",
+    options: ["void", "static", "callback", "banana"],
+    correct: 2,
+    isCorrect: function (choice) {
+        return (choice == this.correct);
+    }
+}
+var questionSeven = {
+    question: "What kind of function is passed as a parameter to another function?",
+    options: ["void", "static", "callback", "banana"],
+    correct: 2,
+    isCorrect: function (choice) {
+        return (choice == this.correct);
+    }
+}
+var questionEight = {
+    question: "What kind of function is passed as a parameter to another function?",
+    options: ["void", "static", "callback", "banana"],
+    correct: 2,
+    isCorrect: function (choice) {
+        return (choice == this.correct);
+    }
+}
+var questionNine = {
+    question: "What kind of function is passed as a parameter to another function?",
+    options: ["void", "static", "callback", "banana"],
+    correct: 2,
+    isCorrect: function (choice) {
+        return (choice == this.correct);
+    }
+}
+var questionTen = {
+    question: "What kind of function is passed as a parameter to another function?",
+    options: ["void", "static", "callback", "banana"],
+    correct: 2,
+    isCorrect: function (choice) {
+        return (choice == this.correct);
+    }
 }
 
-currentQuizObject = questionOne;
+quizObjects = [questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix, questionSeven, questionEight, questionNine, questionTen];
+
+
+// currentQuizObject = questionOne;
 
 function answerButtonClicked(evt) {
     var btnValue = parseInt(evt.target.value);
     if (currentQuizObject.isCorrect(btnValue)) {
-        console.log(btnValue + "correct");
         correctAnswer();
     } else {
         incorrectAnswer();
@@ -52,7 +135,7 @@ function answerButtonClicked(evt) {
 }
 
 function endQuiz() {
-
+    alert("Quiz Ended");
 }
 
 function correctAnswer() {
@@ -71,16 +154,26 @@ function incorrectAnswer() {
     inCorrectTimeout = setTimeout(function () {
         inCorrectElement.setAttribute("style", "visibility: hidden;")
     }, 1000);
+    secondsLeft -= 15;
 }
 
 function loadQuizObject(quiz_object) {
     currentQuizObject = quiz_object;
     questionTextElement.textContent = quiz_object.question;
     for (var i = 0; i < quiz_object.options.length; i++) {
-        answerButtonElements[i].textContent = quiz_object.options[i];
+        answerButtonElements[i].textContent = (i + 1) + ". " + quiz_object.options[i];
     }
 }
 
 function loadNextQuizObject() {
     loadQuizObject(quizObjects.shift());
+}
+
+function gotoHighScores() {
+
+}
+
+function startQuiz() {
+    setTime()
+    loadNextQuizObject();
 }
